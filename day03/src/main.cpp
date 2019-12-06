3e299b818e69fb7b28e54e388d19ef201fca4c42
amdoku <aqpoba@gmail.com>
2019-12-06 22:31:01 +0100

    [Day03, WIP] update

--- day03/src/main.cpp
+++ day03/src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
-#include <type_traits>
+#include <sstream>
 
 // comma seperated template code from: https://stackoverflow.com/a/21838123
 template<class T, char sep=','>
@@ -23,11 +23,62 @@ std::istream& operator>>(std::istream& in, comma_sep<T,sep>& t)
 	return in; //return
 }
 
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T> t) {
+	out << "[" << t.size() << "|";
+	std::copy(t.cbegin(), t.cend(), std::ostream_iterator<T>(out, ","));
+	out << "\b]";
+	return out;
+}
+
+struct Direction {
+	char c;
+};
+
+struct WireToken {
+	struct Direction dir;
+	int length;
+};
+
+std::ostream& operator<<(std::ostream& out, const Direction& t) {
+	return out << t.c;
+}
+
+std::ostream& operator<<(std::ostream& out, const WireToken& t) {
+	return out << "(" << t.dir << "," << t.length << ")";
+}
+
+std::istream& operator>>(std::istream& in, Direction& t) {
+	if (!(in >> t.c))
+		return in;
+	in.clear();
+	return in;
+}
+
+std::istream& operator>>(std::istream& in, WireToken& t) {
+	if (!(in >> t.dir >> t.length))
+		return in;
+	in.clear();
+	return in;
+}
+
+template <typename T>
+std::vector<T> readTokens(std::string& line) {
+	std::stringstream in(line);
+	std::istream_iterator<comma_sep<WireToken>> start(in), end;
+	std::vector<T> tokens{start, end};
+	return tokens;
+}
+
 template <typename T, typename P>
 std::vector<T> readInput(const std::string &path) {
 	std::ifstream input{path};
-	std::istream_iterator<P> start{input}, end;
-	std::vector<T> data{start, end};
+	std::vector<T> data;
+	std::string line;
+	while(std::getline(input, line)) {
+		std::vector<P> tokens = readTokens<P>(line);
+		data.push_back(tokens);
+	}
 	input.close();
 	return data;
 }
@@ -43,9 +94,14 @@ int main(int argc, char const *argv[]) {
 	}
 	// read lines of file
 	// per line create a vector of { orientation, distance } (rad coords)
-	// build set of equidistant points for each wire
+	auto data = readInput<std::vector<WireToken>, WireToken>(argv[1]);
+	log() << "size: " << data.size() << "\n";
+	for(auto&& wire : data) {
+		log() << wire << "\n";
+	}
+	// build set of equidistant points for each wire (points on a grid)
 	// find intersection points
 	// find min with respect to manhattan distance to origin point
 	// ???
-	// Bacon & Profit.
+	// Bacon & Profit.tTtzW
 }
